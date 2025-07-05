08c37530010378ade490b63345d29ed084ec76ca
Kekalbe <rodrigoreiss2005@gmail.com>
2025-07-05 19:23:15 -0300

    Adicionado e reposicionado novas pastas/códigos Teste Importação Swiper

---
+++ Carousel Swiper/sslide.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    effect: "cards",
+    grabCursor: true,
+    loop: false,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    cardsEffect: {
+      perSlideOffset: 35,
+      perSlideRotate: 2,
+      rotate: true,
+      slideShadows: true
+    },
+    on: {
+      click(event) {
+        swiper.slideTo(this.clickedIndex);
+      }
+    }
+  });
+
+  document.querySelectorAll(".swiper-slide").forEach((slide, index) => {
+    slide.addEventListener("click", () => {
+      swiper.slideToLoop(index);
+    });
+  });
+});
